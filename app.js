1ea423738b40ebde7a9cdcbc3fbf1990f739d223
Harsh Vaghasiya <harshvaghasiya@webelight.co.in>
2024-01-10 11:32:53 +0530

    New changes

--- app.js
+++ app.js
@@ -8,25 +8,24 @@ const router = require('./routes/basic');
 const bodyParser = require('body-parser');
 
 
-
+// For use the body parser
 app.use(express.json(), bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Handle Routing
 app.use(router);
 
+// Database connnetion
 const startServer = async () => {
     mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
     console.log("Database connected");
-
-
 }
 
+//  Start server after cheaking connnection
 if (startServer()) {
     app.listen(port, () => {
         console.log("server is running on port " + port);
     });
-
-
 }
 
 
--- controller/controller.js
+++ controller/controller.js
@@ -3,45 +3,53 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 
-
 //  Add new data
 exports.adddata = async (req, res) => {
 
-    const uname = req.body.name;
-    const uemail = req.body.email;
+    const userName = req.body.name;
+    const userEmail = req.body.email;
+    const userAge = req.body.age;
+    const userBdate = req.body.birthdate;
+    const userPassword = req.body.password;
+
     console.log(".............data is ", req.body.name);
     try {
         // throw new Error('test error');
         await model.create(
             {
-                name: uname,
-                email: uemail
+                name: userName,
+                email: userEmail,
+                birthdate: userBdate,
+                age: userAge,
+                password: userPassword,
+
             }
         )
-        res.send("Data Added sucessfully").status(201);
+        res.status(201).send("Data Added sucessfully");
 
     } catch (err) {
         console.log(".................." + err.message);
-        res.send(err.message, 'Data Not Inserted').status(400);
+        // console.log(Object.keys(res))
+        // return new Error("sasdsa")
+        res.send(" Error in data Creation :" + err.message).status(400);
     }
 };
 
 // View the user data
 
-exports.viewuser = async (req,res) => {
+exports.viewuser = async (req, res) => {
     console.log(req.body.name);
-    const username=req.body.name                                     ;
+    const username = req.body.name;
     try {
-        const data = await model.findOne({name:username}).lean();
+        const data = await model.findOne({ name: username }).lean();
         console.log(data);
 
-        res.send("My Name is : " + data.name + "and My Email Addres is :" + data.email ).status(201);
+        res.send("Hear is My Details : \n   Name : " + data.name + "\n Email :" + data.email + "\n Age : " + data.age + "\n Birthdate : " + data.birthdate).status(201);
 
     } catch (err) {
-        res.send(err.message , 'Fetching data is unsucessfull').status(400);
+        res.send(err.message + 'Fetching data is unsucessfull').status(400);
     }
 }
-
 
 // Update the data using id 
 
@@ -50,15 +58,21 @@ exports.updatedata = async (req, res) => {
     const id = req.body.id;
     const newname = req.body.name;
     const newemail = req.body.email;
+    const newAge = req.body.age;
+    const newBdate = req.body.birthdate;
+    const newPassword = req.body.password;
 
     try {
         const data = await model.findByIdAndUpdate(id, {
             name: newname,
-            email: newemail
+            email: newemail,
+            birthdate: newBdate,
+            age: newAge,
+            password: newPassword,
         })
         res.send('Data Updated sucessful').status(201);
     } catch (err) {
-        res.send(err.message, "Error in data Updation").status(400);;
+        res.send(err.message + "Error in data Updation").status(400);
     }
 }
 
@@ -67,9 +81,9 @@ exports.updatedata = async (req, res) => {
 
 exports.deletedata = async (res) => {
     try {
-        const data = await model.findByIdAndDelete('659ce6de2200ce4efc8c5abc');
+        const data = await model.findByIdAndDelete('659e23425779ce3065d1b545');
         res.send("data deleted sucessfully").status(200);;
     } catch (err) {
-        res.send(err.message, 'Data Deletion Unsucessufl').status(400);;
+        res.send(err.message + 'Data Deletion Unsucessufl').status(400);;
     }
 }
